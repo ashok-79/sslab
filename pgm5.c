7cb9e2267bf303af6cb13f4dccfb5d2c983dc82b
ashok-hb <110151417+ashok-hb@users.noreply.github.com>
2023-06-18 15:55:36 +0530

    Update pgm5.c

--- pgm5.c
+++ pgm5.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-char op[2],arg1[5],arg2[5],result[5];
+char op[2],arg1[5],arg2[5],res[5];
 int main()
 {
     FILE *fp1,*fp2;
@@ -10,39 +10,39 @@ int main()
     fp2=fopen("output.txt","w");
     while(!feof(fp1))
     {
-        fscanf(fp1,"%s%s%s%s",result,arg1,op,arg2);
+        fscanf(fp1,"%s%s%s%s",res,arg1,op,arg2);
         if(strcmp(op,"+")==0)
         {
             fprintf(fp2,"\nMOV R0,%s",arg1);
             fprintf(fp2,"\nADD R0,%s",arg2);
-            fprintf(fp2,"\nMOV %s,R0",result);
+            fprintf(fp2,"\nMOV %s,R0",res);
         }
 
         if(strcmp(op,"*")==0)
         {
             fprintf(fp2,"\nMOV R0,%s",arg1);
             fprintf(fp2,"\nMUL R0,%s",arg2);
-            fprintf(fp2,"\nMOV %s,R0",result);
+            fprintf(fp2,"\nMOV %s,R0",res);
         }
 
         if(strcmp(op,"-")==0)
         {
             fprintf(fp2,"\nMOV R0,%s",arg1);
             fprintf(fp2,"\nSUB R0,%s",arg2);
-            fprintf(fp2,"\nMOV %s,R0",result);
+            fprintf(fp2,"\nMOV %s,R0",res);
         }
 
         if(strcmp(op,"/")==0)
         {
             fprintf(fp2,"\nMOV R0,%s",arg1);
             fprintf(fp2,"\nDIV R0,%s",arg2);
-            fprintf(fp2,"\nMOV %s,R0",result);
+            fprintf(fp2,"\nMOV %s,R0",res);
         }
 
         if(strcmp(op,"=")==0)
         {
             fprintf(fp2,"\nMOV R0,%s",arg1);
-            fprintf(fp2,"\nMOV %s,R0",result);
+            fprintf(fp2,"\nMOV %s,R0",res);
         }
     }
     fclose(fp1);
